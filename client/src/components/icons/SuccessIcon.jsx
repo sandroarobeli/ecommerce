52eedb11ab818c1ad188d5d91aba8919a44b9284
Alex Arobelidze <sandroarobeli77@gmail.com>
2023-05-20 23:52:21 -0500

    Feature: added notification alert modal

---
+++ client/src/components/icons/SuccessIcon.jsx
@@ -0,0 +1,17 @@
+export default function SuccessIcon() {
+  return (
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2.0}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
