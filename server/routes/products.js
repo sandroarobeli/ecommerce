b99b533dc687db8a45091f38c9f0022245772eb3
Alex Arobelidze <sandroarobeli77@gmail.com>
2023-05-31 09:11:56 -0500

    Feature: added ability for a logged in user to create/update a review and rating for the product

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { check } = require("express-validator");
 
+const checkAuthorization = require("../utils/checkAuthorization");
 const getAllProducts = require("../controllers/products/getAllProducts");
 const getProductBySlug = require("../controllers/products/getProductBySlug");
+const postReview = require("../controllers/products/postReview");
 
 // Initializing the router object
 const router = express.Router();
@@ -11,5 +14,13 @@ router.get("/pagination/:page", getAllProducts);
 
 // Retrieve product by its slug including comments if any
 router.get("/product/:slug", getProductBySlug);
+
+// Adds review and rating to a specific product. Privileged, requires authorization
+router.post(
+  "/reviews/:productId",
+  [check("content").not().isEmpty().trim().escape()],
+  checkAuthorization,
+  postReview
+);
 
 module.exports = router;
